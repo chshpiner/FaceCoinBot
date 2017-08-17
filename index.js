85f51b2492a0f4554acb0f53969bbae29950ca81
cochva <cochva@gmail.com>
2017-08-17 20:11:57 +0300

    hello

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/webhook/', function (req, res) {
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    url: 'https://graph.facebook.com/v2.10/me/messages',
 	    qs: {access_token:token},
 	    method: 'POST',
 		json: {
