1154dd95fd66c5f32a3fa98d966b356a070f9af3
cochva <cochva@gmail.com>
2017-08-17 20:48:08 +0300

    hello

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-    onsole.log("ccccccccc");
+    console.log("ccccccccc");
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
