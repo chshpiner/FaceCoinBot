4d6a8b60d0dbe8ddafda52f38d523996011bfd8d
cochva <cochva@gmail.com>
2017-08-17 23:48:50 +0300

    hello

--- index.js
+++ index.js
@@ -33,19 +33,25 @@ app.get('/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-    console.log("ccccccccc");
+    console.log(req.body);
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
 	    let sender = event.sender.id
 	    if (event.message && event.message.text) {
 		    let text = event.message.text
+            if (text === 'set selling') {
+  		        setSelling(sender, text);
+  		        continue;
+  	        }
 		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 	    }
     }
     res.sendStatus(200)
 });
-
+function setSelling(sender, text) {
+    console.log(sender);
+}
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
